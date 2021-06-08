98436dfcdfa776c040555a8ea9da7ecb9cf26603
Fernando Iglesias <iglesias.fernandonahuel@gmail.com>
2021-06-08 18:56:00 -0300

    Clase 3 navBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NabVar from './components/navBar'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <NabVar></NabVar>
+      <header className="App-header"> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/navBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function NabVar() {
+    return (
+        <div  style = {{ width : '100%' , height : '30px', background : "grey"}}>
+            Custom NavBar
+        </div>)
+}
+
+export default NabVar;
