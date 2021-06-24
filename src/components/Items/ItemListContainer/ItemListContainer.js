0f6473abb5fcbce793fc72708010f94cf7e6e13b
Fernando Iglesias <iglesias.fernandonahuel@gmail.com>
2021-06-23 21:58:53 -0300

    Correccion entrega Contador con botón

--- src/components/Items/ItemListContainer/ItemListContainer.js
+++ src/components/Items/ItemListContainer/ItemListContainer.js
@@ -3,12 +3,12 @@ import ItemCount from '../ItemCount/ItemCount';
 import ItemList from '../ItemList/ItemList';
 
 function ItemListContainer() {
-  const [cantidad,setCantidad] = useState(0);
+  //const [cantidad,setCantidad] = useState(0);
   const [itemList,setItemList] = useState([]);
   const tempItemList = [{ id:1, title : "Producto 1", description: "Descripcion 1", price: "10", pictureUrl:"https://cdn5.dibujos.net/dibujos/pintados/202008/una-flor-en-un-jarron-naturaleza-flores-11707997.jpg" },{ id:2, title : "Producto 2", description: "Descripcion 2", price: "20", pictureUrl:"https://cdn5.dibujos.net/dibujos/pintados/202008/una-flor-en-un-jarron-naturaleza-flores-11707997.jpg" },{ id:3, title : "Producto 3", description: "Descripcion 3", price: "30", pictureUrl:"https://cdn5.dibujos.net/dibujos/pintados/202008/una-flor-en-un-jarron-naturaleza-flores-11707997.jpg" },{ id:4, title : "Producto 4", description: "Descripcion 4", price: "40", pictureUrl:"https://cdn5.dibujos.net/dibujos/pintados/202008/una-flor-en-un-jarron-naturaleza-flores-11707997.jpg" } ];
 
   const onAdd = (cantidadVar) => {
-    setCantidad(0);
+    //setCantidad(0);
     alert("Se agregaron " + Number(cantidadVar)  +" elementos a tu compra")
   }
 
@@ -30,10 +30,8 @@ function ItemListContainer() {
         <div className="marginBottom1">
            
            <ItemList itemsList={ itemList }/>
-           
          
-
-           <ItemCount initialStock="5" initial={cantidad} onAdd={ onAdd}></ItemCount>
+           <ItemCount initialStock={5} initial={0} onAdd={ onAdd}></ItemCount>
           
         </div>)
 }
